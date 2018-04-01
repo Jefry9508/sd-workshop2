d4c26345dff872436a25b9d143b78b8c3cf0411d
Jefry9508 <jeffrykr95@gmail.com>
2018-04-01 18:52:51 -0500

    Add rabbit.sh

---
+++ A00320232/rabbit.sh
@@ -0,0 +1,11 @@
+service rabbitmq-server start
+rabbitmqctl add_vhost /sensu
+rabbitmqctl add_user sensu password
+rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"
+rabbitmq-plugins enable rabbitmq_management
+chown -R rabbitmq:rabbitmq /var/lib/rabbitmq/
+rabbitmqctl add_user test test
+rabbitmqctl set_user_tags test administrator
+rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
+sudo systemctl enable rabbitmq-server
+sudo systemctl start rabbitmq-server
